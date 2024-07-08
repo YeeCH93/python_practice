e1af14467bd66a0bdaca36e18bd2fda24d3facd5
YeeCH93 <chatrapat.yee93@gmail.com>
2024-07-08 20:25:33 +0700

    P1_BlockLetter

---
+++ P1_BlockLetters.py
@@ -0,0 +1,23 @@
+# William Yee
+# Dog Lover
+my_name = """
+W   W  Y   Y
+W   W   Y Y 
+W   W    Y  
+W W W    Y  
+WW WW    Y  
+WW WW    Y  
+W   W    Y  
+"""
+print(my_name)
+
+about_me = """
+DDDD   L     
+D   D  L    
+D   D  L    
+D   D  L    
+D   D  L    
+D   D  L    
+DDDD   LLLLL
+"""
+print(about_me)
