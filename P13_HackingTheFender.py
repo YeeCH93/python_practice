b87145094bc0a6dc277c76bb576ed513fca9163d
YeeCH93 <chatrapat.yee93@gmail.com>
2024-07-16 13:56:22 +0700

    P13_HackingTheFender

---
+++ P13_HackingTheFender.py
@@ -0,0 +1,50 @@
+# 1, 12 import module
+import csv
+import json
+
+# 2 create empty list
+compromised_users = []
+
+# 3,4,5,6,7 open read password file
+with open('passwords.csv') as password_file:
+  password_csv = csv.DictReader(password_file)
+  for password_row in password_csv:
+    compromised_users.append(password_row['Username'])
+
+# 8,9,10,11 write file
+with open('compromised_users.txt', 'w') as compromised_user_file:
+  for user in compromised_users:
+    compromised_user_file.write(user + '\n')
+
+# 13,14,15 open write mode json file
+with open('boss_message.json', 'w') as boss_message:
+  boss_message_dict = {
+    "recipient": "The Boss",
+    "message": "Mission Success"
+  }
+  json.dump(boss_message_dict, boss_message)
+
+# 16,17,18 remove existing file
+slash_null_sig = """
+ _  _     ___   __  ____             
+/ )( \   / __) /  \(_  _)            
+) \/ (  ( (_ \(  O ) )(              
+\____/   \___/ \__/ (__)             
+ _  _   __    ___  __ _  ____  ____  
+/ )( \ / _\  / __)(  / )(  __)(    \ 
+) __ (/    \( (__  )  (  ) _)  ) D ( 
+\_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+        ____  __     __   ____  _  _ 
+ ___   / ___)(  )   / _\ / ___)/ )( \
+(___)  \___ \/ (_/\/    \\___ \) __ (
+       (____/\____/\_/\_/(____/\_)(_/
+ __ _  _  _  __    __                
+(  ( \/ )( \(  )  (  )               
+/    /) \/ (/ (_/\/ (_/\             
+\_)__)\____/\____/\____/
+"""
+with open('new_passwords.csv', 'w') as new_passwords_obj:
+  new_passwords_obj.write(slash_null_sig)
+
+
+
